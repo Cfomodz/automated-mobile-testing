6179251ff788df32896980b7984793bb0df0cee1
Leonardo Dias <leonadias@gmail.com>
2023-12-18 21:22:41 -0300

    Inclusão de novos cenários Add Pacote Delete Pacote

--- entrega_final/app_utils.py
+++ entrega_final/app_utils.py
@@ -1,25 +1,36 @@
 from appium.webdriver.common.appiumby import AppiumBy
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import NoSuchElementException
 import time
 
 class AppUtils:
     def __init__(self, driver):
         self.driver = driver
 
+    def debug_print(self, message):
+        if getattr(self, 'debug', False):
+            print(f"DEBUG: {message}")
+
     def login(self, username, password):
         """Realiza o login no aplicativo."""
+        # Localiza e clica no botão de login
         el_botao_login = self.driver.find_element(by=AppiumBy.XPATH, value="//android.widget.Button[@text=\"Faça login!\"]")
         el_botao_login.click()
 
+        # Preenche o campo de usuário
         el_usuario = self.driver.find_element(by=AppiumBy.ID, value="br.com.muambator.android:id/login_username")
         el_usuario.click()
         el_usuario.send_keys(username)
 
+        # Preenche o campo de senha
         el_senha = self.driver.find_element(by=AppiumBy.ID, value="br.com.muambator.android:id/login_password")
         el_senha.click()
         el_senha.send_keys(password)
 
+        # Confirma o login
         el_login_confirmar = self.driver.find_element(by=AppiumBy.ID, value="br.com.muambator.android:id/login_confirm")
         el_login_confirmar.click()
 
@@ -37,7 +48,7 @@ class AppUtils:
         el_ola_usuario = self.driver.find_element(by=AppiumBy.XPATH, value="//android.widget.TextView[@text=\"Olá, lfsdias\"]")
         el_ola_usuario.click()
 
-        # Verifica e valida mensagem
+        # Verifica e valida a mensagem antes de prosseguir
         el_mensagem = WebDriverWait(self.driver, 10).until(
             EC.visibility_of_element_located((AppiumBy.ID, "android:id/message"))
         )
@@ -45,7 +56,7 @@ class AppUtils:
         mensagem = el_mensagem.text
         assert mensagem == "Deseja realmente sair da sua conta?", "Mensagem 'Deseja realmente sair da sua conta?' não encontrada"
 
-        # Clique para Sair
+        # Clica para confirmar o logout
         print("Clicando no botão de logout")
         el_logout = WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable((AppiumBy.ID, "android:id/button1"))
@@ -66,7 +77,7 @@ class AppUtils:
             print("Clicando na mensagem de erro para fechar")
             el_error_message.click()
 
-            # Clicar no botão para fechar a janela de mensagem
+            # Clica no botão para fechar a janela de mensagem
             print("Esperando o botão de fechar aparecer")
             el_close_button = WebDriverWait(self.driver, 10).until(
                 EC.element_to_be_clickable((AppiumBy.ID, "android:id/button1"))
@@ -80,64 +91,104 @@ class AppUtils:
     def add_new_packet(self, package_code, package_name):
         """Inclui um novo pacote."""
 
-        # Clicar no botão "Incluir pacote"
+        # Clica no botão "Incluir pacote"
         el_add_package = WebDriverWait(self.driver, 10).until(
             EC.element_to_be_clickable((AppiumBy.ACCESSIBILITY_ID, "Incluir pacote"))
         )
         el_add_package.click()
 
-        # Preencher o código do pacote
+        # Preenche o código do pacote
         el_code = self.driver.find_element(by=AppiumBy.ID, value="br.com.muambator.android:id/package_form_text_code")
         el_code.click()
         el_code.send_keys(package_code)
 
-        # Preencher o nome do pacote
+        # Preenche o nome do pacote
         el_name = self.driver.find_element(by=AppiumBy.ID, value="br.com.muambator.android:id/package_form_text_name")
         el_name.click()
         el_name.send_keys(package_name)
 
-        # Confirmar a inclusão
+        # Confirma a inclusão
         el_confirm = self.driver.find_element(by=AppiumBy.ACCESSIBILITY_ID, value="Confirmar")
         el_confirm.click()
 
-        time.sleep(20)
+        # Aguarda a inclusão ser processada
+        WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((AppiumBy.XPATH, "//android.view.View[@resource-id=\"mys-content\"]/android.view.View[2]/android.widget.TextView"))
+        ).click()
+
+    def interact_with_advertisement(self):
+        """Interage com a publicidade, se presente."""
+        try:
+            # Aguardar a exibição da página de publicidade
+            el_advertisement = WebDriverWait(self.driver, 60).until(
+                EC.presence_of_element_located((By.XPATH,
+                                                "//android.view.View[@resource-id=\"mys-content\"]/android.view.View[2]/android.widget.TextView"))
+            )
+
+            # Verificar se o elemento está presente antes de clicar
+            if el_advertisement.is_displayed():
+                el_advertisement.click()
+
+                # Tentar clicar em "Pular vídeo" ou no ícone de fechar
+                possible_elements = [
+                    {"locator": (By.XPATH, "//android.widget.TextView[@text=\"Pular vídeo\"]"), "name": "Pular vídeo"},
+                    {"locator": (By.XPATH,
+                                 "//android.view.View[@resource-id=\"mys-content\"]/android.view.View[2]/android.widget.TextView"),
+                     "name": "Ícone de Fechar"}
+                ]
+
+                for element_info in possible_elements:
+                    try:
+                        element = WebDriverWait(self.driver, 10).until(
+                            EC.element_to_be_clickable(element_info["locator"])
+                        )
+                        element.click()
+                        print(f"Clicou em {element_info['name']}")
+                        break  # Se clicou em algum elemento, sair do loop
+                    except TimeoutException:
+                        print(f"Elemento {element_info['name']} não encontrado ou não é clicável.")
+
+        except TimeoutException:
+            # Tratar a exceção e continuar ou sinalizar uma falha, conforme necessário
+            print("Elemento de publicidade não encontrado após 60 segundos.")
+        except NoSuchElementException:
+            # Tratar a exceção e continuar ou sinalizar uma falha, conforme necessário
+            print("Elementos da publicidade não encontrados ou ações não foram necessárias")
 
     def delete_package(self, package_name):
         """Exclui um pacote."""
-
-        # Verificar se o pacote está na lista
         try:
+            # Verifica se o pacote está na lista antes de prosseguir
             el_package = WebDriverWait(self.driver, 10).until(
                 EC.element_to_be_clickable((AppiumBy.XPATH,
                                             f"//android.widget.TextView[@text='{package_name}']"))
             )
-
-            # Se o pacote estiver visível, continue com a exclusão
+            # Se o pacote estiver visível, continua com a exclusão
             el_package.click()
 
-            # Clicar no ícone "Mais opções"
+            # Clica no ícone "Mais opções"
             el_more_options = WebDriverWait(self.driver, 10).until(
                 EC.element_to_be_clickable((AppiumBy.ACCESSIBILITY_ID, "Mais opções"))
             )
             el_more_options.click()
 
-            # Escolher a opção "Excluir"
+            # Escolhe a opção "Excluir"
             el_delete_option = WebDriverWait(self.driver, 10).until(
                 EC.element_to_be_clickable((AppiumBy.XPATH,
                                             "//android.widget.TextView[@resource-id=\"br.com.muambator.android:id/title\" and @text=\"Excluir\"]"))
             )
             el_delete_option.click()
 
-            # Aguardar a tela de confirmação de exclusão
+            # Aguarda a tela de confirmação de exclusão
             el_confirm_title = WebDriverWait(self.driver, 10).until(
                 EC.visibility_of_element_located((AppiumBy.ID, "br.com.muambator.android:id/alertTitle"))
             )
 
-            # Clicar no botão "Confirmar"
+            # Clica no botão "Confirmar"
             el_confirm_button = self.driver.find_element(by=AppiumBy.ID, value="android:id/button1")
             el_confirm_button.click()
 
-            # Aguardar alguns segundos para a exclusão ser processada
+            # Aguarda alguns segundos para a exclusão ser processada
             time.sleep(5)
 
         except TimeoutException:
--- entrega_final/test_suite_muambator.py
+++ entrega_final/test_suite_muambator.py
@@ -1,13 +1,15 @@
 import unittest
+import time
+
 from appium import webdriver
 from appium.options.common.base import AppiumOptions
 from appium.webdriver.common.appiumby import AppiumBy
-from selenium.webdriver.support.ui import WebDriverWait  # Adicione esta linha
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from app_utils import AppUtils
 
-class TesteMuambatorApp(unittest.TestCase):
+class TestMuambatorApp(unittest.TestCase):
 
     def setUp(self):
         # Configurar as capacidades desejadas do Appium
@@ -38,40 +40,84 @@ class TesteMuambatorApp(unittest.TestCase):
         if hasattr(self, 'debug') and self.debug:
             print(f"DEBUG: {message}")
 
-    def test_login_com_sucesso_e_navegacao_para_preferencias(self):
-        self.debug = True  # Altere False para não imprimir informações de depuração
+    def test_01_login_sucess(self):
+        """Testa o login com sucesso e navegação para a tela de Preferências."""
+        self.debug = True
 
-        self.debug_print("Cenário 1: Login")
-        self.app_utils.login("lfsdias", "teste123")
+        # Dados do Login
+        username = "lfsdias"
+        password = "teste123"
+
+        # Realiza o Login
+        self.app_utils.login(username, password)
+
+        #Navega para a tela de preferências
         self.app_utils.navigate_to_preferences()
 
-        # Verificar o nome do usuário na tela "Preferências"
+        # Verifica o nome do usuário na tela "Preferências"
         self.debug_print("Verificando o nome do usuário na tela de Preferências")
-        el_ola_usuario = self.driver.find_element(by=AppiumBy.XPATH, value="//android.widget.TextView[@text=\"Olá, lfsdias\"]")
+        el_ola_usuario = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((AppiumBy.XPATH, f"//android.widget.TextView[@text='Olá, {username}']"))
+        )
         self.assertTrue(el_ola_usuario.is_displayed(), "Nome do usuário não encontrado na tela de Preferências")
 
-    def test_logout_e_retorno_para_tela_inicial(self):
+    def test_02_falha_login_senha_invalida(self):
+        """Testa o login com falha devido à senha inválida."""
         self.debug = True
-        self.debug_print("Cenário 2: Logout")
 
-        self.app_utils.login("lfsdias", "teste123")
+        # Dados do Login
+        username = "lfsdias"
+        password = "senha incorreta"
+
+        # Realiza o Login
+        self.app_utils.login(username, password)
+
+        # Fecha a janela da mensagem de erro
+        self.app_utils.close_error_message()
+
+        # Verifica se a mensagem de erro foi fechada corretamente
+        self.debug_print("Verificando se a mensagem de erro foi fechada corretamente")
+        el_page_login = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, "//android.widget.TextView[@text=\"Login\"]"))
+        )
+        self.assertTrue(el_page_login.is_displayed(), "A mensagem de erro não foi fechada corretamente")
+    def test_03_falha_login_usuario_invalido(self):
+        """Testa o login com falha devido à usuário inválido."""
+        self.debug = True
+
+        # Dados do Login
+        username = "Kx9"
+        password = "teste123"
+
+        # Realiza o Login
+        self.app_utils.login(username, password)
+
+        # Fecha a janela da mensagem de erro
+        self.app_utils.close_error_message()
+
+        # Verifica se a mensagem de erro foi fechada corretamente
+        self.debug_print("Verificando se a mensagem de erro foi fechada corretamente")
+        el_page_login = WebDriverWait(self.driver, 10).until(
+            EC.visibility_of_element_located((By.XPATH, "//android.widget.TextView[@text=\"Login\"]"))
+        )
+        self.assertTrue(el_page_login.is_displayed(), "A mensagem de erro não foi fechada corretamente")
+
+    def test_04_logout_sucess(self):
+        """Testa o logout e retorno para a tela inicial."""
+        self.debug = True
+
+        # Dados do Login
+        username = "lfsdias"
+        password = "teste123"
+
+        # Realiza o Login
+        self.app_utils.login(username, password)
+
+        # Navega para a tela de preferências
         self.app_utils.navigate_to_preferences()
 
-        el_ola_usuario = self.driver.find_element(by=AppiumBy.XPATH, value="//android.widget.TextView[@text=\"Olá, lfsdias\"]")
-        el_ola_usuario.click()
-
-        # Verificar mensagem e validar
-        el_mensagem = self.driver.find_element(by=AppiumBy.ID, value="android:id/message")
-        self.assertTrue(el_mensagem.is_displayed(), "Mensagem 'Deseja realmente sair da sua conta?' não encontrada")
-        mensagem = el_mensagem.text
-        self.assertEqual(mensagem, "Deseja realmente sair da sua conta?")
-
-        # Clique para Sair
-        self.debug_print("Clicando no botão de logout")
-        el_logout = WebDriverWait(self.driver, 10).until(
-            EC.element_to_be_clickable((By.ID, "android:id/button1"))
-        )
-        el_logout.click()
+        # Chama a função de logout
+        self.app_utils.logout()
 
         # Verifica se volta para tela inicial
         self.debug_print("Verificando se retornou para a tela inicial após o logout")
@@ -79,6 +125,63 @@ class TesteMuambatorApp(unittest.TestCase):
             EC.visibility_of_element_located((By.XPATH, "//android.widget.TextView[@text=\"Nova conta\"]"))
         )
         self.assertTrue(el_botao_login_apos_logout.is_displayed(), "Não retornou para a tela inicial após o logout")
+
+    def test_05_add_new_package_success(self):
+        """Testa a inclusão de um novo pacote com sucesso."""
+        self.debug = True
+        self.debug_print("Cenário 5: Incluir Pacote com Sucesso")
+
+        # Dados do Login e pacote
+        username = "lfsdias"
+        password = "teste123"
+        package_code = "NM027033020BR"
+        package_name = "Pacote Automatizado"
+
+        # Realiza o Login
+        self.debug_print("Realiza Login")
+        self.app_utils.login(username, password)
+        time.sleep(5)
+
+        # Incluir pacote
+        self.debug_print("Inclui pacote")
+        self.app_utils.add_new_packet(package_code, package_name)
+
+        # Aguardar a exibição da página de publicidade e interagir com ela
+        self.debug_print("Aguardando a exibição da página de publicidade")
+        self.app_utils.interact_with_advertisement()
+
+        time.sleep(5)  # Aguarda um tempo após interagir com a publicidade
+
+        # Verificar se o pacote foi adicionado corretamente
+        self.debug_print("Verificando se o pacote foi adicionado corretamente")
+        el_added_package = self.driver.find_element(by=AppiumBy.XPATH,
+                                                    value=f"//android.widget.TextView[@text=\"{package_name}\"]")
+        self.assertTrue(el_added_package.is_displayed(), "Pacote não encontrado na lista")
+
+    def test_06_delete_package_success(self):
+        """Testa a exclusão de um pacote com sucesso."""
+        self.debug = True
+        self.debug_print("Cenário 6: Excluir Pacote com Sucesso")
+
+        # Dados do Login e pacote
+        username = "lfsdias"
+        password = "teste123"
+        package_name_to_delete = "Pacote Automatizado"
+
+        # Realiza o Login
+        self.debug_print("Realiza Login")
+        self.app_utils.login(username, password)
+        time.sleep(5)
+
+        # Excluir pacote
+        self.debug_print("Excluindo o pacote")
+        self.app_utils.delete_package(package_name_to_delete)
+
+        # Verificar se o pacote foi removido corretamente
+        self.debug_print("Verificando se o pacote foi removido corretamente")
+        el_deleted_package = self.driver.find_elements(by=AppiumBy.XPATH,
+                                                       value=f"//android.widget.TextView[@text=\"{package_name_to_delete}\"]")
+        self.assertFalse(el_deleted_package, "Pacote encontrado na lista após exclusão")
 
 
 if __name__ == '__main__':
